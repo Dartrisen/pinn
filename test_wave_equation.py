f2095110395b55196ffe2af81ea839e4d056ae46
dartrisen <calculating.scientist@gmail.com>
2024-05-27 01:48:03 -0700

    test_gradients function has added

--- test_wave_equation.py
+++ test_wave_equation.py
@@ -13,3 +13,15 @@ class TestGradients(unittest.TestCase):
         self.t = grids[1].flatten().reshape(-1, 1)
 
         self.model = Network(num_hidden=2, dim_hidden=3)
+
+    def test_gradients(self):
+        eps = 1e-4
+        f_x_derivative = (f(self.model, self.x + eps, self.t) - f(self.model, self.x - eps, self.t)) / (2 * eps)
+        f_value = f(self.model, self.x, self.t)
+        f_x_autoderivative = grad(f_value, self.x, order=1)
+        is_matching_x = torch.allclose(f_x_derivative.T, f_x_autoderivative.T, atol=1e-2, rtol=1e-2)
+        self.assertTrue(is_matching_x)
+
+
+if __name__ == '__main__':
+    unittest.main()
